9d2d509fac584d9cf3a8a869d30f49d36917cca6
Willy_Lin <willy_lin@asus.com>
2025-01-13 16:47:59 +0800

    [RAG] HomeWork 1

--- student_assignment.py
+++ student_assignment.py
@@ -5,12 +5,32 @@ from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import JsonOutputParser
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
-    pass
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature']
+    )
+    llm.bind(response_format={"type": "json_object"})
+    json_parser = JsonOutputParser()
+    format_instructions = json_parser.get_format_instructions()
+
+    message = HumanMessage(
+            content=[
+                {"type": "text", "text": question},
+            ]
+    )
+    response = llm.invoke([question + f"{format_instructions}"])
+    json_output = json_parser.invoke(response)
+    return json_output
     
 def generate_hw02(question):
     pass
@@ -38,3 +58,9 @@ def demo(question):
     response = llm.invoke([message])
     
     return response
+
+query = "2024年台灣10月紀念日有哪些?"
+print(generate_hw01(question=query))
+
+# query = "what is azure openai service?"
+# demo(question=query).pretty_print()
